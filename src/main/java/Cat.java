5938c27142a1a39a65724865781dd7fea91e23c3
mmohchin18 <mohima.mohchin@gmail.com>
2021-09-29 15:14:28 +0100

    comment cat class

--- src/main/java/Cat.java
+++ src/main/java/Cat.java
@@ -1,43 +1,43 @@
-import java.time.LocalDate;
-
-
-public class Cat extends Mammal {
-
-   // private static final java.lang.Object LocalDate = null;
-    //////////////////Attributes///////////////////////////
-    private String breed;
-    private LocalDate dateOfBirth;
-    private String name;
-
-    /////////////////Constructors//////////////////////////
-    public Cat() {
-        this((String) "Moggy");
-    }
-
-    public Cat(String name) {
-        // this(name, LocalDate.now());
-    }
-
-//    public <LocalDate> Cat(String name, LocalDate dOB) {
-//        this(name, LocalDate.now(), "DSH");
+//import java.time.LocalDate;
+//
+//
+//public class Cat extends Mammal {
+//
+//   // private static final java.lang.Object LocalDate = null;
+//    //////////////////Attributes///////////////////////////
+//    private String breed;
+//    private LocalDate dateOfBirth;
+//    private String name;
+//
+//    /////////////////Constructors//////////////////////////
+//    public Cat() {
+//        this((String) "Moggy");
 //    }
-
-    public Cat(String name, LocalDate dOB, String breed) {
-        this.name = name;
-        this.dateOfBirth = dOB;
-        this.breed = breed;
-    }
-
-    @Override
-    public void breathe() {
-        
-    }
-
-    @Override
-    public void reproduce() {
-
-    }
-
-
-    private Object System;
-}
+//
+//    public Cat(String name) {
+//        // this(name, LocalDate.now());
+//    }
+//
+////    public <LocalDate> Cat(String name, LocalDate dOB) {
+////        this(name, LocalDate.now(), "DSH");
+////    }
+//
+//    public Cat(String name, LocalDate dOB, String breed) {
+//        this.name = name;
+//        this.dateOfBirth = dOB;
+//        this.breed = breed;
+//    }
+//
+//    @Override
+//    public void breathe() {
+//
+//    }
+//
+//    @Override
+//    public void reproduce() {
+//
+//    }
+//
+//
+//    private Object System;
+//}
